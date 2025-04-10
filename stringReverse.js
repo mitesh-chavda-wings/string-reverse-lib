6a7d53f33ebb54d148aab56fb9e0b08639b998ac
mitesh-chavda-wings <mitesh.c@wingstechsolutions.com>
2025-04-10 13:04:54 +0530

    Add capitalizeString function to stringReverse.js

--- stringReverse.js
+++ stringReverse.js
@@ -9,6 +9,13 @@ function countString(str) {
     return str.length;
 }
 
+function capitalizeString(str) {
+    if (typeof str !== 'string') throw new Error('Input must be a string');
+    if (str.length === 0) return str;
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 // Export both functions for browser use
 window.reverseString = reverseString;
 window.countString = countString;
+window.capitalizeString = capitalizeString;
