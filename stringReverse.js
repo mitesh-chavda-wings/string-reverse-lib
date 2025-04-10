d92e07ff78b1bac245e342a1b96cefe754e20819
mitesh-chavda-wings <mitesh.c@wingstechsolutions.com>
2025-04-10 13:15:49 +0530

    Add class

--- stringReverse.js
+++ stringReverse.js
@@ -1,21 +1,21 @@
 // stringReverse.js
-function reverseString(str) {
-    if (typeof str !== 'string') throw new Error('Input must be a string');
-    return str.split('').reverse().join('');
+class StringUtils {
+    reverseString(str) {
+        if (typeof str !== 'string') throw new Error('Input must be a string');
+        return str.split('').reverse().join('');
+    }
+
+    countString(str) {
+        if (typeof str !== 'string') throw new Error('Input must be a string');
+        return str.length;
+    }
+
+    capitalizeString(str) {
+        if (typeof str !== 'string') throw new Error('Input must be a string');
+        if (str.length === 0) return str;
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
 }
 
-function countString(str) {
-    if (typeof str !== 'string') throw new Error('Input must be a string');
-    return str.length;
-}
-
-function capitalizeString(str) {
-    if (typeof str !== 'string') throw new Error('Input must be a string');
-    if (str.length === 0) return str;
-    return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
-// Export both functions for browser use
-window.reverseString = reverseString;
-window.countString = countString;
-window.capitalizeString = capitalizeString;
+// Export for browser use
+window.StringUtils = StringUtils;
