3ca16b5ac9f754f12ebf3737219e949888e31b9a
mitesh-chavda-wings <mitesh.c@wingstechsolutions.com>
2025-04-10 12:37:42 +0530

    Add string count funciton

--- stringReverse.js
+++ stringReverse.js
@@ -1,14 +1,14 @@
 // stringReverse.js
 function reverseString(str) {
-    if (typeof str !== 'string') {
-        throw new Error('Input must be a string');
-    }
+    if (typeof str !== 'string') throw new Error('Input must be a string');
     return str.split('').reverse().join('');
 }
 
-// Export the function for use as a module
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = reverseString;
-} else {
-    window.reverseString = reverseString; // Make it available globally in browsers
+function countString(str) {
+    if (typeof str !== 'string') throw new Error('Input must be a string');
+    return str.length;
 }
+
+// Export both functions for browser use
+window.reverseString = reverseString;
+window.countString = countString;
